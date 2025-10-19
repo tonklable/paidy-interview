e2bc8e7e8328e4e58b236813c794f6972fe5b87d
tonklable <tonklalor2544@gmail.com>
2025-10-19 13:06:04 +0900

    Change Unauthorized type

--- forex-mtl/src/main/scala/forex/http/auth/TokenAuth.scala
+++ forex-mtl/src/main/scala/forex/http/auth/TokenAuth.scala
@@ -3,15 +3,12 @@ package forex.http.auth
 import cats.data.OptionT
 import cats.effect.Sync
 import forex.http.rates.Converters.GetApiErrorResponseOps
-import org.http4s.{ HttpRoutes, Request, Response }
-import org.http4s.dsl.Http4sDsl
+import org.http4s.Status.Unauthorized
+import org.http4s.{HttpRoutes, Request, Response}
 import org.typelevel.ci.CIString
 
 object TokenAuth {
   def apply[F[_]: Sync](expectedToken: String)(routes: HttpRoutes[F]): HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F] {}
-    import dsl._
-
     HttpRoutes { req: Request[F] =>
       val tokenHeader = req.headers.get(CIString("token")).map(_.head.value)
 
