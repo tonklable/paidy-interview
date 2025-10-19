672cec36929bc323848c97cdc95fd7c3083db26d
tonklable <tonklalor2544@gmail.com>
2025-10-19 13:26:18 +0900

    Change to case pattern for RedisInterpreter

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/RedisInterpreter.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/RedisInterpreter.scala
@@ -1,15 +1,14 @@
 package forex.services.rates.interpreters
 
 import cats.Monad
-import cats.data.EitherT
-import cats.implicits.toBifunctorOps
+import cats.implicits.{catsSyntaxApplicativeId, catsSyntaxEitherId, toFlatMapOps}
 import forex.config.RedisConfig
 import forex.domain.Rate
 import io.circe.parser.decode
 import forex.services.rates.RedisAlgebra
 import forex.services.rates.errors.Error.SystemError
 import forex.services.rates.errors._
-import forex.services.rates.interpreters.RateUtils.{ toJson, toRate }
+import forex.services.rates.interpreters.RateUtils.{toJson, toRate}
 import io.circe.syntax.EncoderOps
 
 class RedisInterpreter[F[_]: Monad](client: RedisClient[F], config: RedisConfig) extends RedisAlgebra[F] {
@@ -17,14 +16,15 @@ class RedisInterpreter[F[_]: Monad](client: RedisClient[F], config: RedisConfig)
   private val key = "rates"
 
   override def getAll: F[Error Either List[Rate]] =
-    (for {
-      jsonOption <- EitherT.liftF(client.get(key))
-      json <- EitherT.fromOption[F](jsonOption, SystemError(s"Cache miss for key $key"): Error)
-      jsonRates <- EitherT.fromEither[F](
-                    decode[List[RateJson]](json).leftMap(e => SystemError(s"Parse error: ${e.getMessage}"): Error)
-                  )
-      rates = jsonRates.flatMap(toRate)
-    } yield rates).value
+    client.get(key).flatMap {
+      case Some(json) => decode[List[RateJson]](json) match {
+        case Left(err) => (SystemError(s"Parse error: ${err.getMessage}"): Error).asLeft[List[Rate]].pure[F]
+        case Right(rateJson) => rateJson.flatMap(toRate).asRight[Error].pure[F]
+    }
+      case None =>
+        (SystemError(s"Cache miss"): Error).asLeft[List[Rate]].pure[F]
+    }
+
   override def store(rates: List[Rate]): F[Unit] = {
     val jsonRates = rates.map(toJson)
     val json      = jsonRates.asJson.noSpaces
