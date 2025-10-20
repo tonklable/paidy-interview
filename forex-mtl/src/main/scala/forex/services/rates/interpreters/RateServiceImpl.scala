9eeef41e0106d3845298d5fe568095b23018206e
tonklable <tonklalor2544@gmail.com>
2025-10-21 02:10:17 +0900

    Update rate limit logic

--- forex-mtl/src/main/scala/forex/services/rates/interpreters/RateServiceImpl.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/RateServiceImpl.scala
@@ -26,7 +26,7 @@ class RateServiceImpl[F[_]: Sync](
           if (passed)
             api.getAll.flatMap {
               case Right(rates) =>
-                cache.store(rates) >> guard.release >>
+                guard.release >> cache.store(rates) >>
                   Sync[F].delay(Timestamp.now).map(findOrDivideRate(rates, pair, _))
               case Left(error) => guard.release >> error.asLeft[Rate].pure[F]
             } else
